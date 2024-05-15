73fd9cd0bca2f804579c65e4b4a8fc39d6f30c51
HoangIka <86615257+HoangIka@users.noreply.github.com>
2024-05-16 06:00:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-require('dotenv/config');
+require('dotenv').config();
 const {Client,GatewayIntentBits} = require('discord.js');
 const client = new Client({
 	intents: [
